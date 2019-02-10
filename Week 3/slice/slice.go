94614504e95821595f9b60ae650464766086381a
Pierre Baudin <pierrejean.baudin@hotmail.com>
2019-02-10 16:09:36 +0800

    slice manip update with while loop

--- Week 3/slice/slice.go
+++ Week 3/slice/slice.go
@@ -2,36 +2,42 @@ package main
 
 import "fmt"
 import "sort"
+import "strconv"
 
 func main() {
 	// Define user input variables
-	var input int
-	// Define slice
-	userSlice := make([]int, 3)
+	var input string
 
-	for i := 0; i < 4; i++ {
+	// Define slice
+	userSlice := make([]int, 1)
+
+	for input != "x" {
 		// Prompt user to enter integer
 		fmt.Print("Press x to exit of Enter an integer: ")
 
 		// Read user input
 		fmt.Scanln(&input)
 
+		// Convert input to intuser
+		intuser, err := strconv.Atoi(input)
+		if err != nil {
+			// handle error
+		}
+
 		// Loop to compare value
 		for i, v := range userSlice {
-			if v <= input {
-				userSlice[i] = input
+			if v == intuser {
+				userSlice[i] = v
 				break
 			} else {
-				userSlice = append(userSlice, input)
+				userSlice = append(userSlice, intuser)
 				break
 			}
 		}
 
 		sort.Ints(userSlice)
-
+		// Print user input
+		fmt.Print(userSlice)
+		fmt.Printf("len=%d", len(userSlice))
 	}
-
-	// Print user input
-	fmt.Print(userSlice)
-	fmt.Printf("len=%d", len(userSlice))
 }
